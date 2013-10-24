ee687bc24f0f0efa46c5168603446d333bce017e
Nicolas Liautaud <nicolas.liautaud@gmail.com>
2013-10-24 20:47:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# Pico Pages Images
+
+Access to the images of the current page in [Pico CMS](http://pico.dev7studios.com).
+
+### Installation
+
+Copy `pico_pages_images.php` to the `plugins/` directory of your Pico Project.
+
+### Usage
+
+Loop trough the Twig `images` variable in your theme, or access to an image directly.
+
+```twig
+{% for image in images %}
+	<img src="{{ image.url }}" alt="{{ image.name }}" {{ image.size }}>
+{% endfor %}
+
+{{ images[0].name }}
+```
+
+The images are nicely stored next to the page file, or in a folder using the same name.
+
+	path/
+		foo/
+			foo-image_01.jpg
+			foo-image_02.png
+			index.md
+		bar/
+			bar-image_01.jpg
+			bar-image_02.gif
+		bar.md
+
+
+The images contains the following data :
+
+	image.url		:	http://mysite.com/page/image.jpg
+	image.path	 	:	page/image.jpg
+	image.name	 	:	image
+	image.ext		:	jpg
+	image.width	 	:	800
+	image.height	:	600
+	image.size	 	:	width="800" height="600"
---
+++ pico_pages_images.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Access to the images of the current page folder with {{ images }} in Pico CMS.
+ *
+ * @author  Nicolas Liautaud
+ * @link http://nliautaud.fr
+ * @link http://pico.dev7studios.com
+ * @license http://opensource.org/licenses/MIT
+ */
+class Pico_Pages_Images
+{
+	private $path;
+
+	// Pico hooks ---------------
+
+	/**
+	 * Register the images path.
+	 */
+	public function request_url(&$url)
+	{
+		$this->path = $this->format($url);
+	}
+
+	/**
+	 * Register the images data in {{ images }} Twig variable.
+	 */
+	public function before_render(&$twig_vars, &$twig)
+	{
+		$twig_vars['images'] = $this->images_list($twig_vars['base_url']);
+	}
+
+
+	// CORE ---------------
+
+	/**
+	 * Return the images path based on the given path.
+	 */
+	private function format($path)
+	{
+		$is_index = strripos($path, 'index') === strlen($path)-5;
+		if($is_index) return substr($path, 0, -5);
+		elseif($path) $path .= '/';
+
+		return $path;
+	}
+	/**
+	 * Return the list and infos of images in the current directory.
+	 */
+	private function images_list($base_url)
+	{
+		$data = array();
+		$pattern = '*.{[jJ][pP][gG],[jJ][pP][eE][gG],[pP][nN][gG],[gG][iI][fF]}';
+		$images = glob(CONTENT_DIR . $this->path . $pattern, GLOB_BRACE);
+
+		foreach ($images as $path)
+		{
+			list($dirname, $basename, $ext, $filename) = array_values(pathinfo($path));
+			list($width, $height, $type, $size, $mime) = getimagesize($path);
+
+			$data[] = array (
+				'url' => $base_url . '/content/' . $this->path . $basename,
+				'path' => $dirname,
+				'name' => $filename,
+				'ext' => $ext,
+				'width' => $width,
+				'height' => $height,
+				'size' => $size
+			);
+		}
+		return $data;
+	}
+}
+?>
